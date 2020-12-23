1183ebd65e3d5d4054083f046aefe68683e1b92f
Randy Tozuka <kazm1208@me.com>
2020-12-24 07:13:03 +0900

    24th Dec 2020

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,37 +19,36 @@ class BookingsController < ApplicationController
     @user = current_user
     @booking_date = Time.zone.parse(params[:booking][:date])
     @booking_slot = params[:booking][:slot]
-    # binding.pry
     # 予約ができる日は本日以降の未来とする
     if @booking_date <  Date.today
       flash[:danger]= "Head for the future!"
-      redirect_to new_booking_path
+      redirect_to new_booking_path and return
+    end
     # 日曜（0）か土曜（6）は予約をさせない。→@booking_dateはstring型なのでdate型に変換し、wdayで曜日を出す
     # elsif Date.strptime(@booking_date).wday == 0 || Date.strptime(@booking_date).wday == 6
-    elsif @booking_date.wday == 0 || @booking_date.wday == 6
+    if @booking_date.wday == 0 || @booking_date.wday == 6
       flash[:danger]= "Dont' be a slave of your job!"
-      redirect_to new_booking_path
-    else
-    # binding.pry
-    #同じ人が同じ日に予約を入れる事を防ぐ…昼食は一日一回であろうと想定
-      if Booking.where(user_id: @user.id).where(date: @booking_date).any?
-        flash[:danger]= "Double booking in the the day! Please check."
-        redirect_to root_path
-    #同じ日の同じスロットに入る人数の調整…現在はテストとして1人以上予約が入ることを阻止
-      elsif Booking.where(date:@booking_date).where(slot:@booking_slot).count >= 1
-            flash[:danger]= "That slot is fully occupied! Please try other slot."
-            redirect_to new_booking_path
-      else
-        if current_user.bookings.create(booking_params)
-          flash[:success]= "Successfully booked"
-          redirect_to '/'
-        else
-          flash[:danger]= "Your booking failed"
-          redirect_to '/bookings/new'
-        end
-      end
+      redirect_to new_booking_path and return
     end
-  end
+    #同じ人が同じ日に予約を入れる事を防ぐ…昼食は一日一回であろうと想定
+    if Booking.where(user_id: @user.id).where(date: @booking_date).any?
+      flash[:danger]= "Double booking in the the day! Please check."
+      redirect_to root_path and return
+    end
+    #同じ日の同じスロットに入る人数の調整…現在はテストとして1人以上予約が入ることを阻止
+    if Booking.where(date:@booking_date).where(slot:@booking_slot).count >= 1
+      flash[:danger]= "That slot is fully occupied! Please try other slot."
+      redirect_to new_booking_path and return
+    end
+    #うまくいったらとそうでなかったら
+    if current_user.bookings.create(booking_params)
+      flash[:success]= "Successfully booked"
+      redirect_to root_path and return
+    else
+      flash[:danger]= "Your booking failed"
+      redirect_to '/bookings/new' and return
+    end
+  end# of create
 
   def edit
     @booking = current_user.bookings.find(params[:id])
@@ -68,7 +67,6 @@ class BookingsController < ApplicationController
       flash[:danger]= "Dont' be a slave of your job!"
       redirect_to root_path
     else
-    # binding.pry
     #同じ日の同じスロットに入る人数の調整…現在はテストとして1人以上予約が入ることを阻止
       if Booking.where(date:@booking_date).where(slot:@booking_slot).count >= 1
             flash[:danger]= "That slot is fully occupied! Please try other slot."
@@ -83,7 +81,7 @@ class BookingsController < ApplicationController
         end
       end
     end
-  end
+  end# of update
 
   def destroy
     @booking = current_user.bookings.find(params[:id])
@@ -102,13 +100,3 @@ class BookingsController < ApplicationController
     end
 
 end# of class
-
-#メモ
-# @booking_count1 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"11:30~11:45").count
-# @booking_count2 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"11:45~12:00").count
-# @booking_count3 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:00~12:15").count
-# @booking_count4 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:15~12:30").count
-# @booking_count5 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:30~12:45").count
-# @booking_count6 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"12:45~13:00").count
-# @booking_count7 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"13:00~13:15").count
-# @booking_count8 = Booking.where("date LIKE?", "%#{@booking_date}%").where(slot:"13:15~13:30").count
