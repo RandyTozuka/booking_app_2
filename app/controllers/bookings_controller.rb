bb34b070b6e448f18eed6002def3df942090d095
Randy Tozuka <kazm1208@me.com>
2020-12-30 13:46:56 +0900

    update create_update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,8 @@
 class BookingsController < ApplicationController
 
-#binding.pry
-
   def index
     if current_user && Booking.any?
+      # 参考：　https://teratail.com/questions/121843
       @bookings = current_user.bookings.where('date >= ?', Date.today)
     end
   end
@@ -55,30 +54,31 @@ class BookingsController < ApplicationController
 
   def update
     @user = current_user
-    @booking_date = params[:booking][:date]
+    @booking_date = Time.zone.parse(params[:booking][:date])
     @booking_slot = params[:booking][:slot]
     # 予約ができる日は本日以降の未来とする
-    if @booking_date <  Date.today.to_s
+    if @booking_date <  Date.today
       flash[:danger]= "Head for the future!"
-      redirect_to root_path
+      redirect_to root_path and return
+    end
     # 日曜（0）か土曜（6）は予約をさせない。→@booking_dateはstring型なのでdate型に変換し、wdayで曜日を出す
-    elsif Date.strptime(@booking_date).wday == 0 || Date.strptime(@booking_date).wday == 6
+    if @booking_date.wday == 0 || @booking_date.wday == 6
       flash[:danger]= "Dont' be a slave of your job!"
-      redirect_to root_path
-    else
+      redirect_to root_path and return
+    end
+    #同じ人が同じ日に予約を入れる事を防ぐ機能は入れない→同じ日の違う時間への変更はあり得るため
     #同じ日の同じスロットに入る人数の調整…現在はテストとして1人以上予約が入ることを阻止
-      if Booking.where(date:@booking_date).where(slot:@booking_slot).count >= 1
-            flash[:danger]= "That slot is fully occupied! Please try other slot."
-            redirect_to root_path
-      else
-        if current_user.bookings.update(booking_params)
-          flash[:success]= "Successfully changed!"
-          redirect_to '/'
-        else
-          flash[:danger]= "Your booking changing failed"
-          redirect_to '/bookings/new'
-        end
-      end
+    if Booking.where(date:@booking_date).where(slot:@booking_slot).count >= 1
+      flash[:danger]= "That slot is fully occupied! Please try other slot."
+      redirect_to root_path and return
+    end
+    #うまくいったらとそうでなかったら
+    if current_user.bookings.update(booking_params)
+      flash[:success]= "Successfully edited"
+      redirect_to root_path and return
+    else
+      flash[:danger]= "Your booking failed"
+      redirect_to root_path and return
     end
   end# of update
 
