802bd140e72e23d3f55aaabe58e808200395012d
Randy Tozuka <kazm1208@me.com>
2021-01-12 05:35:33 +0900

    12 Jan 2021

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# # ユーザーを99人生成する
+# 99.times do |n|
+#   email = "example-#{n+1}@test.com"
+#   password = "password"
+#   User.create!(email: email, password: password,)
+# end
+#
+# # ユーザーの一部を対象に予約を生成する
+# users = User.order(:created_at).take(10)
+# 50.times do
+#   users.each { |user| user.bookings.create!(date:*** ), (slot:*** }
+# end
