69b02f17fd967ddd9952657bf5b688d6e7fb53ae
Randy Tozuka <kazm1208@me.com>
2021-01-14 05:54:21 +0900

    routes.rbのget 'bookings/show' を無効化

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'bookings/show'
+  # get 'bookings/show'
   resources :bookings
   root 'bookings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
