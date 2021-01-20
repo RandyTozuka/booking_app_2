c6665438b73544894f24e26137c95768e0a9ea82
Randy Tozuka <kazm1208@me.com>
2021-01-20 12:31:58 +0900

    seeds.rbにtimeframesテーブルのslot、admin user を作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# # ユーザーを99人生成する
-# 99.times do |n|
-#   email = "example-#{n+1}@test.com"
-#   password = "password"
-#   User.create!(email: email, password: password,)
-# end
-#
-# # ユーザーの一部を対象に予約を生成する
-# users = User.order(:created_at).take(10)
-# 50.times do
-#   users.each { |user| user.bookings.create!(date:*** ), (slot:*** }
-# end
+# timeframesにslotを入れる
+Timeframe.create!(slot: '11:30~11:45')
+Timeframe.create!(slot: '11:45~12:00')
+Timeframe.create!(slot: '12:00~12:15')
+Timeframe.create!(slot: '12:15~12:30')
+Timeframe.create!(slot: '12:30~12:45')
+Timeframe.create!(slot: '12:45~13:00')
+Timeframe.create!(slot: '13:00~13:15')
+Timeframe.create!(slot: '13:15~13:30')
+
+# admin user を設定する
+# 参考：https://qiita.com/day-1/items/8eb903fcf083b30a17a4
+# 参考：https://stackoverflow.com/questions/12418584/seeding-users-with-devise-in-ruby-on-rails
+user = User.new(
+  email: 'testman0@test.com',
+  password: 'testman0',
+  password_confirmation: 'testman0',
+  admin: "true"
+)
+user.save
+
+# 複数人の user を作る
+# 参考:https://qiita.com/takehanKosuke/items/79a66751fe95010ea5ee
+5.times do |n|
+  user = User.new(
+    email: "testman#{n + 1}@test.com",
+    password: "testman#{n + 1}",
+    password_confirmation: 'testman#{n + 1}'
+  )
+  user.save
+  end
