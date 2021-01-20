ebdcd745ae97150cb5dd8c7f5de929598fc387fb
Randy Tozuka <kazm1208@me.com>
2021-01-20 12:45:28 +0900

    seeds.rbにtimeframesテーブルのslot、admin user を作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,11 +29,11 @@ user.save
 
 # 複数人の user を作る
 # 参考:https://qiita.com/takehanKosuke/items/79a66751fe95010ea5ee
-5.times do |n|
-  user = User.new(
-    email: "testman#{n + 1}@test.com",
-    password: "testman#{n + 1}",
-    password_confirmation: 'testman#{n + 1}'
-  )
-  user.save
-  end
+# 5.times do |n|
+#   user = User.new(
+#     email: "testman#{n + 1}@test.com",
+#     password: "testman#{n + 1}",
+#     password_confirmation: 'testman#{n + 1}'
+#   )
+#   user.save
+#   end
